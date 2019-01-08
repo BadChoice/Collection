60af811643ae0e03dd57bd4c3c2573c5d8dfa80e
Jordi Puigdellívol <jordi@gloobus.net>
2019-01-08 17:07:09 +0100

    Adding osx

---
+++ Collection OSX/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Collection OSX
+//
+//  Created by Jordi Puigdellívol on 08/01/2019.
+//  Copyright © 2019 Revo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Collection OSX/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Collection OSX
+//
+//  Created by Jordi Puigdellívol on 08/01/2019.
+//  Copyright © 2019 Revo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
